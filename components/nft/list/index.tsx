c5723a8f1572da0522a01e686a804038d76e5eb6
Richard Hong <rhong24@gmail.com>
2022-12-19 17:27:45 -0600

    defining types of meta json of nft, refactoring code to dynamically load in information with mapping instead of hardcoding examples

--- components/nft/list/index.tsx
+++ components/nft/list/index.tsx
@@ -1,13 +1,22 @@
 import { FunctionComponent } from "react"
+import { NftMeta } from '../../../types/nft'
 import NftItem from '../item'
 
-const NftList: FunctionComponent = () => {
+type NftListProps = {
+  nfts: NftMeta[]
+}
+
+const NftList: FunctionComponent<NftListProps> = ({ nfts }) => {
 
   return (
     <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-      <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-        <NftItem />
-      </div>
+      {nfts.map(nft => 
+        <div key={nft.image} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+          <NftItem 
+            item={nft}
+          />
+        </div>
+      )}
     </div>
   )
 }
---
+++ types/nft.ts
@@ -0,0 +1,13 @@
+export type Trait = "attack" | "defense" | "speed";
+
+export type NftAttribute = {
+  trait_type: Trait;
+  value: string;
+}
+
+export type NftMeta = {
+  name: string;
+  description: string;
+  image: string;
+  attributes: NftAttribute[];
+}

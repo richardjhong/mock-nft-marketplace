8468d63514fc643cd4370dfed55d518b56eb2fe2
Richard Hong <rhong24@gmail.com>
2022-12-19 20:47:35 -0600

    modifying starter code to strip out unused links, adding ActiveLink, removing unused styling within globals, adding profile page & pages/nft & comonents/link

---
+++ pages/nft/create.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next"
+import { BaseLayout } from "../../components"
+
+const NftCreate: NextPage = () => {
+  return (
+    <BaseLayout>
+      Nft Create
+    </BaseLayout>
+  )
+}
+
+export default NftCreate
